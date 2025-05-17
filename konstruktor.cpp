79e0bfe77605e5f8af5b721c616a1f01bc270eff
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-17 16:26:18 +0800

    commit12

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -10,11 +10,24 @@ class mahasiswa{
     mahasiswa();
     mahasiswa(int);
     mahasiswa(string);
-    mahasiswa(int iNim, string, iNama);
+    mahasiswa(int iNim, string iNama);
     void cetak();
 };
 
 mahasiswa :: mahasiswa(){
     nim = 0;
     nama = "";
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim=iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama=iNama;
+}
+
+mahasiswa ::  mahasiswa(int iNim, string iNama){
+    nim=iNim;
+    nama=iNama;
 }
