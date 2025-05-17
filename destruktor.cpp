4fd2278b6f6dfb3881632b8ca3aab0b3b48d36c1
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-17 15:45:16 +0800

    commit5

--- destruktor.cpp
+++ destruktor.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class angka
-{
+class angka{
 private:
     int *arr;
     int panjang;
-
 public:
     angka(int); // constructor
     ~angka();   // destructor
     void cetakdata();
     void isidata();
 };
+// definisi member Function
+angka::angka(int i){ // constructor
+    panjang = i;
+    arr = new int[i];
+    isidata();
+}
