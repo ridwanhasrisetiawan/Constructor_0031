a424ac5d6e9d8d827756fff34c0338c438452ebc
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-17 17:00:21 +0800

    commit20

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -29,3 +29,20 @@ void mahasiswa::printALL(){
     cout<<"Nama = "<<nama<<endl;
     cout<<endl;
 }
+
+int main(){
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+    mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
+    mahasiswa mhs3("Andi Janu");
+    mahasiswa mhs4("Joko Wahono");
+
+    mhs1.printALL();
+    mhs2.printALL();
+    mhs3.printALL();
+    mhs4.printALL();
+
+    cout<<"akses dari luar object = "<<mahasiswa::getNim()<<endl; //mengakses nim melalui static member function "getNim"
+
+    return 0;
+}
