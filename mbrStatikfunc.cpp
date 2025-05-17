d18dfb102205bfdb7ad2eb6b06fedaf7a605ea6d
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-17 16:44:50 +0800

    commmit17

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa{
+    private:
+    static int nim;
+    public:
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+    static void setNim(int pNim){nim=pNim; /*Definisi Funcation*/}
+    static int getNim(){return nim; /*Definisi Funcation*/}
+
+    mahasiswa(string pnama):nama(pnama){setID();}
+};
